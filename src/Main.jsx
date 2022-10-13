8b79e8dbd92e5e7b074092e06704cff04ba7a6c3
“ARustamA” <rakhmetyanov@gmail.com>
2022-10-13 18:33:33 +0600

    12

---
+++ src/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import style from './Main.module.scss';
+
+export const Main = () => {
+
+
+  return (
+    <div className={style.main}>
+      <div className={style.content}>
+        <h1>Main</h1>
+      </div>
+      
+    </div>
+  )
+}
+
